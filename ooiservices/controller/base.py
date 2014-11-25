bcdc5fd2a0e7729138d01caf7b4fd48bc6ffa149
Edna Donoughe <ednapa01@gmail.com>
2014-11-25 08:48:39 -0500

    add cache model and erdap controller/model

--- ooiservices/controller/base.py
+++ ooiservices/controller/base.py
@@ -58,7 +58,11 @@ class BaseController(Resource):
         return make_response('', 204)
 
     def response_HTTP404(self):
-        return make_response(jsonify({'error': 'Not Found' } ), 404)
+        return make_response(jsonify({'error': 'Not Found'} ), 404)
+
+    def response_HTTP500(self):
+        return make_response(jsonify({'error': 'internal server error'} ), 500)
+
 
     def process_args(self,model, args, key):
         result = None
